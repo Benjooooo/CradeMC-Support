181d8d64dfeaf0a0145500d4b4bd56ab8006e497
Benjamin <30363535+Benjooooo@users.noreply.github.com>
2018-03-09 18:26:00 +0100

    Add files via upload

---
+++ net/crademc/benjamin/labymod/player/PlayerInfo.java
@@ -0,0 +1,14 @@
+package net.crademc.benjamin.labymod.player;
+
+public class PlayerInfo {
+
+    private String minigame = "Lobby";
+
+    public String getMinigame() {
+        return minigame;
+    }
+
+    public void setMinigame(String minigame) {
+        this.minigame = minigame;
+    }
+}
